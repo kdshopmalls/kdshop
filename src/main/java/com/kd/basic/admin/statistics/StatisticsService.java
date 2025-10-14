8521e8392a35b58fa4b3d7a817a6e434c5a49789
kwangwon <kk4778@gmail.com>
2025-10-14 17:02:32 +0900

    카테고리등록 수정삭제 기능 / 통계추가

---
+++ src/main/java/com/kd/basic/admin/statistics/StatisticsService.java
@@ -0,0 +1,59 @@
+package com.kd.basic.admin.statistics;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class StatisticsService {
+
+	private final StatisticsMapper statisticsMapper;
+	
+	public List<OrderAmount> monthlysales_statistics(int year) {
+		return statisticsMapper.monthlysales_statistics(year);
+	}
+	
+	public List<Map<String, Object>> monthlysales_statistics2(int year) {
+		return statisticsMapper.monthlysales_statistics2(year);
+	}
+	
+	public List<Map<String, Object>> getDailyStatistics(String date) {
+		return statisticsMapper.getDailyStatistics(date);
+	}
+	
+	public List<Map<String, Object>> getHourlyStatistics(String start_date, String end_date) {
+		return statisticsMapper.getHourlyStatistics(start_date, end_date);
+	}
+	
+	public List<Map<String, Object>> getWeeklyStatistics(String start_date, String end_date) {
+		return statisticsMapper.getWeeklyStatistics(start_date, end_date);
+	}
+	
+	public List<Map<String, Object>> getMonthlyStatistics(String year) {
+		return statisticsMapper.getMonthlyStatistics(year);
+	}
+	
+	public int getMonthlyCount() {
+		return statisticsMapper.getMonthlyCount();
+	}
+	public int getMonthlySalesTotal() {
+		return statisticsMapper.getMonthlySalesTotal();
+	}
+	public int getTotalProductCount() {
+		return statisticsMapper.getTotalProductCount();
+	}
+	public int getSoldOutProductCount() {
+		return statisticsMapper.getSoldOutProductCount();
+	}
+	public List<Map<String, Object>> getMonthlyOrderCategoryStats(){
+		return statisticsMapper.getMonthlyOrderCategoryStats();
+	}
+	
+	public List<Map<String, Object>> getThisWeekOrderAmount(){
+		return statisticsMapper.getThisWeekOrderAmount();
+	}
+}
