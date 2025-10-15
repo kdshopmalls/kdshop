bf445b23353cf5cf137c05962f48b0718d8b4d16
kwangwon <kk4778@gmail.com>
2025-10-15 16:35:10 +0900

    데시보드 추가

--- src/main/java/com/kd/basic/admin/login/AdminLoginController.java
+++ src/main/java/com/kd/basic/admin/login/AdminLoginController.java
@@ -1,12 +1,18 @@
 package com.kd.basic.admin.login;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.kd.basic.admin.statistics.StatisticsService;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +26,7 @@ public class AdminLoginController {
 	
 	private final AdminLoginService adminLoginService;
 	private final PasswordEncoder passwordEncoder;
-	
+	private final StatisticsService statisticsService;
 	@GetMapping("/")
 	public String ad_login() {
 		return "/admin/ad_login";
@@ -55,13 +61,55 @@ public class AdminLoginController {
 			
 		return "redirect:" + url;
 	}
-	@GetMapping("ad_menu")
-	public String menu() {
+	@GetMapping("/ad_menu")
+	public void ad_menu(Model model) throws Exception{
+		model.addAttribute("monthlyCount", statisticsService.getMonthlyCount());
+		model.addAttribute("monthlySalesTotal", statisticsService.getMonthlySalesTotal());
+		model.addAttribute("totalProductCount", statisticsService.getTotalProductCount());
+		model.addAttribute("soldOutProductCount", statisticsService.getSoldOutProductCount());
+		List<Map<String, Object>> monthlyOrderCategoryStats = statisticsService.getMonthlyOrderCategoryStats();
+
+	    List<String> labels1 = monthlyOrderCategoryStats.stream()
+	            .map((Map<String, Object> stat) -> (String) stat.get("primary_category_name"))
+	            .collect(Collectors.toList());
+
+	    List<Integer> data1 = monthlyOrderCategoryStats.stream()
+	            .map((Map<String, Object> stat) -> {
+	                Object value = stat.get("total_orders");
+	                if (value instanceof Number) {
+	                    return ((Number) value).intValue();
+	                } else {
+	                    return 0;
+	                }
+	            })
+	            .collect(Collectors.toList());
+
+	    model.addAttribute("labels1", labels1);
+	    model.addAttribute("data1", data1);
+	    
+	    /*             */
+	    
+	    List<Map<String, Object>> thisWeekOrderAmount = statisticsService.getThisWeekOrderAmount();
+
+	    List<String> labels2 = thisWeekOrderAmount.stream()
+	            .map((Map<String, Object> stat) -> String.valueOf(stat.get("order_date")))
+	            .collect(Collectors.toList());
+
+	    List<Integer> data2 = thisWeekOrderAmount.stream()
+	            .map((Map<String, Object> stat) -> {
+	                Object value = stat.get("daily_total_price");
+	                if (value instanceof Number) {
+	                    return ((Number) value).intValue();
+	                } else {
+	                    return 0;
+	                }
+	            })
+	            .collect(Collectors.toList());
+
+	    model.addAttribute("labels2", labels2);
+	    model.addAttribute("data2", data2);
 		
-		return "admin/ad_menu";
-	}
-	
-		
+	}	
 }
 
 
