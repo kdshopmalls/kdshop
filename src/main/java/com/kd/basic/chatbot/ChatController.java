89ffb04db7d246969e7ee28a06fa36b80f038765
Jun <pimang123@naver.com>
2025-10-17 10:29:05 +0900

    챗봇,카카오로그인 추가

---
+++ src/main/java/com/kd/basic/chatbot/ChatController.java
@@ -0,0 +1,35 @@
+package com.kd.basic.chatbot;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/chatapi/*")
+@CrossOrigin(origins = "*")
+public class ChatController {
+
+    private final OpenAIService openAIService;
+
+    
+    @GetMapping("/chatForm")
+    public void chatForm() {
+    	log.info("테스트");
+    }
+    
+    @GetMapping("/chat")
+    public ResponseEntity<String> ask(@RequestParam String question) {
+    	ResponseEntity<String> entity = null;
+    	
+    	entity = new ResponseEntity<String>(openAIService.handleUserMessage(question), HttpStatus.OK);
+    	
+    	return entity;
+    }
+}
+
