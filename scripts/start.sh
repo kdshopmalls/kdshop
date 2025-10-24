89e07ae10c99e75902abd5e01b222d3faa79b868
kwangwon <kk4778@gmail.com>
2025-10-24 15:49:26 +0900

    자동테스트3

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # start.sh
 
-PROJECT="ezenshop-1.0.0"
-PROJECT_NAME=ezenshop
+PROJECT="kdshop-1.0.0"
+PROJECT_NAME=kdshop
 PROJECT_FULL_PATH="/home/ec2-user/app1/step1"
 JAR_FILE="$PROJECT_FULL_PATH/$PROJECT.jar"
 LOG_PATH="$PROJECT_FULL_PATH/logs"
@@ -22,7 +22,7 @@ echo "$NOW_DATETIME > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 
 cd $PROJECT_FULL_PATH
 
-cp $PROJECT_FULL_PATH/ezenshop/target/*.jar $JAR_FILE
+cp $PROJECT_FULL_PATH/kdshop/target/*.jar $JAR_FILE
 #sudo cp /home/ec2-user/app1/step1/ezenshop/target/ezenshop-1.0.0.jar.jar ezenshop-1.0.0.jar
 
 cd $PROJECT_FULL_PATH
